7e189fbfaa20349b9c60d2f523853cb84376e6c5
Semih DAGHAN <daghansoftware@hotmail.com>
2023-01-10 11:00:04 +0300

    IGenericRepository

--- JsonWebTokens.Core/Repositories/IGenericRepository.cs
+++ JsonWebTokens.Core/Repositories/IGenericRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,10 @@ namespace JsonWebTokens.Core.Repositories
     public interface IGenericRepository<TEntity> where TEntity:class
     {
         Task<TEntity> GetByIdAsync(int id);
-        Task<IQueryable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+        TEntity Update(TEntity entity);
     }
 }
