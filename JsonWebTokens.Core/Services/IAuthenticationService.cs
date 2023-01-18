cdde22db120784762759b04e9c883af5ef459518
Semih Dağhan <daghanex@hotmail.com>
2023-01-18 23:33:11 +0300

    IAuthenticationService interface'i oluşturuldu.

---
+++ JsonWebTokens.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,21 @@
+﻿using JsonWebTokens.Core.Models.Dtos;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonWebTokens.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+
+        Task<Response<TokenDto>>CreateTokenByRefreshAsync(string refreshToken);
+
+        Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+
+        Task<Response<ClientTokenDto>> GetTokenByClientAsync(ClientLoginDto clientLoginDto);
+    }
+}
