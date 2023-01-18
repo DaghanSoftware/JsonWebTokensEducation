8632438f22ee9ca0f138296feeaca1b30dfbed72
Semih Dağhan <daghanex@hotmail.com>
2023-01-19 00:12:34 +0300

    IUserService oluşturuldu.

---
+++ JsonWebTokens.Core/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using JsonWebTokens.Core.Models.Dtos;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonWebTokens.Core.Services
+{
+    public interface IUserService
+    {
+        Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<Response<UserAppDto>> GetUserByNameAsync(string userName);
+    }
+}
