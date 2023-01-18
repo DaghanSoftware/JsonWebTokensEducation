ed486e5e54e8650a3cda8206dc18d2bb74f46cd1
Semih Dağhan <daghanex@hotmail.com>
2023-01-18 23:23:36 +0300

    ITokenService interface'i ve Client sınıfı oluşturuldu.

---
+++ JsonWebTokens.Core/Configuration/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace JsonWebTokens.Core.Configuration
+{
+    public class Client
+    {
+        public string Id { get; set; }
+
+        public string Secret { get; set; }
+
+        public List<string> Audiences { get; set; }
+    }
+}
---
+++ JsonWebTokens.Core/Services/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using JsonWebTokens.Core.Configuration;
+using JsonWebTokens.Core.Models.Dtos;
+using JsonWebTokens.Core.Models.Entities;
+
+namespace JsonWebTokens.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userapp);
+
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
