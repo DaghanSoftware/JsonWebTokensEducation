9af0cfce3d4a99b8b8fd03ec7cf1b74e483e680f
Semih Dağhan <daghanex@hotmail.com>
2023-01-22 23:59:26 +0300

    AuthenticationService

--- JsonWebTokens.API/Program.cs
+++ JsonWebTokens.API/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
-builder.Services.Configure<Client>(builder.Configuration.GetSection("Client"));
+builder.Services.Configure<List<Client>>(builder.Configuration.GetSection("Client"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ JsonWebTokens.Service/Services/AuthenticationService.cs
@@ -0,0 +1,50 @@
+﻿using JsonWebTokens.Core.Configuration;
+using JsonWebTokens.Core.Models.Dtos;
+using JsonWebTokens.Core.Models.Entities;
+using JsonWebTokens.Core.Repositories;
+using JsonWebTokens.Core.Services;
+using JsonWebTokens.Core.UnitOfWork;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Dtos;
+
+namespace JsonWebTokens.Service.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly List<Client> _client;
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<UserApp> _userManager;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericRepository<UserRefreshToken> _userRefreshTokenService;
+
+        public AuthenticationService(IOptions<List<Client>> optionsClient, ITokenService tokenService, UserManager<UserApp> userManager, IUnitOfWork unitOfWork, IGenericRepository<UserRefreshToken> userRefreshTokenService)
+        {
+            _client = optionsClient.Value;
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _unitOfWork = unitOfWork;
+            _userRefreshTokenService = userRefreshTokenService;
+        }
+
+        public Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<TokenDto>> CreateTokenByRefreshAsync(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<ClientTokenDto>> GetTokenByClientAsync(ClientLoginDto clientLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
