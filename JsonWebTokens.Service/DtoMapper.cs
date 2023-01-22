e5259e8fb03157e2c7449c0c36ed5f3a5d91f592
Semih Dağhan <daghanex@hotmail.com>
2023-01-22 18:11:04 +0300

    AutoMapper

---
+++ JsonWebTokens.Service/DtoMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using JsonWebTokens.Core.Models.Dtos;
+using JsonWebTokens.Core.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonWebTokens.Service
+{
+    internal class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
