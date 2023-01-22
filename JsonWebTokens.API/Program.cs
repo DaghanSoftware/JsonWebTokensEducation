b3193f023724ff80140239a734d0a28460b8282d
Semih Dağhan <daghanex@hotmail.com>
2023-01-22 23:49:48 +0300

    Option Patter (Client)

--- JsonWebTokens.API/Program.cs
+++ JsonWebTokens.API/Program.cs
@@ -1,3 +1,4 @@
+using JsonWebTokens.Core.Configuration;
 using Microsoft.Extensions.Configuration;
 using SharedLibrary.Configurations;
 
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
+builder.Services.Configure<Client>(builder.Configuration.GetSection("Client"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
