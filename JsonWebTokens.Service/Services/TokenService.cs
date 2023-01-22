9bd325287ff8820b4931e0711c99c84acd61ed97
Semih Dağhan <daghanex@hotmail.com>
2023-01-22 23:25:49 +0300

    Token Payload'ına Claim Ekleme

--- JsonWebTokens.Service/Services/TokenService.cs
+++ JsonWebTokens.Service/Services/TokenService.cs
@@ -4,10 +4,12 @@ using JsonWebTokens.Core.Models.Entities;
 using JsonWebTokens.Core.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
 using SharedLibrary.Configurations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,6 +34,19 @@ namespace JsonWebTokens.Service.Services
             using var rnd = RandomNumberGenerator.Create();
             rnd.GetBytes(numberByte);
             return Convert.ToBase64String(numberByte);
+        }
+
+        private IEnumerable<Claim> GetClaim(UserApp userApp, List<String> audiences)
+        {
+            var userlist = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier,userApp.Id),
+                new Claim(JwtRegisteredClaimNames.Email,userApp.Email),
+                new Claim(ClaimTypes.Name,userApp.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+            userlist.AddRange(audiences.Select(x => new Claim(JwtRegisteredClaimNames.Aud, x)));
+            return userlist;
         }
 
         public TokenDto CreateToken(UserApp userapp)
