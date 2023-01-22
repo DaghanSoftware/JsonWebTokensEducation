3aa3ca444cbaf61ff74429a9189587e0964b96eb
Semih Dağhan <daghanex@hotmail.com>
2023-01-22 23:14:43 +0300

    TokenService1

---
+++ JsonWebTokens.Service/Services/TokenService.cs
@@ -0,0 +1,39 @@
+﻿using JsonWebTokens.Core.Configuration;
+using JsonWebTokens.Core.Models.Dtos;
+using JsonWebTokens.Core.Models.Entities;
+using JsonWebTokens.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonWebTokens.Service.Services
+{
+    class TokenService : ITokenService
+    {
+
+        private readonly UserManager<UserApp> _userManager;
+
+        private readonly CustomTokenOption _tokenOptions;
+
+        public TokenService(UserManager<UserApp> userManager, IOptions<CustomTokenOption> options)
+        {
+            _userManager = userManager;
+            _tokenOptions = options.Value;
+        }
+
+        public TokenDto CreateToken(UserApp userapp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
