77e687371ecc2ff465d2336228a12a2dadf921bd
Semih Dağhan <daghanex@hotmail.com>
2023-01-23 08:03:10 +0300

    AuthenticationService-3

--- JsonWebTokens.Service/Services/AuthenticationService.cs
+++ JsonWebTokens.Service/Services/AuthenticationService.cs
@@ -5,6 +5,7 @@ using JsonWebTokens.Core.Repositories;
 using JsonWebTokens.Core.Services;
 using JsonWebTokens.Core.UnitOfWork;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using SharedLibrary.Dtos;
 
@@ -27,9 +28,40 @@ namespace JsonWebTokens.Service.Services
             _userRefreshTokenService = userRefreshTokenService;
         }
 
-        public Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto)
+        public async Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto)
         {
-            throw new NotImplementedException();
+            if (loginDto ==null)
+            {
+                throw new ArgumentNullException(nameof(loginDto));
+            }
+
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user == null) { return Response<TokenDto>.Fail("Email or Password is wrong", 400,true); }
+
+            if (!await _userManager.CheckPasswordAsync(user, loginDto.Password)) 
+            {
+                return Response<TokenDto>.Fail("Email or Password is wrong", 400, true);
+            }
+
+            var token = _tokenService.CreateToken(user);
+            var userRefreshToken = await _userRefreshTokenService.Where(x=>x.UserId==user.Id).SingleOrDefaultAsync();
+            if (userRefreshToken == null) 
+            {
+                await _userRefreshTokenService.AddAsync(new UserRefreshToken
+                {
+                    UserId = user.Id,
+                    Code = token.RefreshToken,
+                    Expiration = token.RefreshTokenExpiration
+                }
+                ); 
+            }
+            else
+            {
+               userRefreshToken.Code = token.RefreshToken;
+                userRefreshToken.Expiration = token.RefreshTokenExpiration;
+            }
+            await _unitOfWork.CommitAsync();
+            return Response<TokenDto>.Success(token, 200);
         }
 
         public Task<Response<TokenDto>> CreateTokenByRefreshAsync(string refreshToken)
