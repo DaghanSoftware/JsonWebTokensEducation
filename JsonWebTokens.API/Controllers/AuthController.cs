b52e503369121e79bfbc46617768c75d8246efc4
Semih Dağhan <daghanex@hotmail.com>
2023-02-19 01:22:54 +0300

    AuthController

---
+++ JsonWebTokens.API/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using JsonWebTokens.Core.Models.Dtos;
+using JsonWebTokens.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JsonWebTokens.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class AuthController : CustomBaseController
+    {
+        private readonly IAuthenticationService _authenticationService;
+        public AuthController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateToken(LoginDto loginDto)
+        {
+            var result = await _authenticationService.CreateTokenAsync(loginDto);
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            var result =  _authenticationService.CreateTokenByClient(clientLoginDto);
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RevokeRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            var result = await _authenticationService.RevokeRefreshToken(refreshTokenDto.RefreshToken);
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTokenByRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            var result = await _authenticationService.CreateTokenByRefreshAsync(refreshTokenDto.RefreshToken);
+            return ActionResultInstance(result);
+        }
+    }
+}
