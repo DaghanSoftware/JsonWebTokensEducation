be09a74b6905aa0670d2aa0219264b128767416b
Semih Dağhan <daghanex@hotmail.com>
2023-02-19 01:29:09 +0300

    UserController

---
+++ JsonWebTokens.API/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using JsonWebTokens.Core.Models.Dtos;
+using JsonWebTokens.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JsonWebTokens.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class UserController : CustomBaseController
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserDto createUserDto)
+        {
+            return ActionResultInstance(await _userService.CreateUserAsync(createUserDto));
+        }
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            return ActionResultInstance(await _userService.GetUserByNameAsync(HttpContext.User.Identity.Name));
+        }
+    }
+}
